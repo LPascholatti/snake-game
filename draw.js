7d8537e3c1b7ceafa858f92c9ae0d4627e1bf644
LPascholatti <lucaspascholatti@gmail.com>
2020-01-09 15:54:21 +0100

    Commiting js files for Snake and background Draw

---
+++ draw.js
@@ -0,0 +1,14 @@
+const canvas = document.querySelector(".canvas");
+const ctx = canvas.getContext("2d");
+const scale = 10;
+const rows = canvas.height / scale;
+const columns = canvas.width / scale;
+
+var snake;
+
+(function setup() {
+  snake = new Snake();
+  snake.draw();
+}());
+
+// console.log(canvas);
