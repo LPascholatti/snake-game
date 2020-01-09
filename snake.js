5e0374b03cf2b875ac5f2de3ec265d9ac5c119cb
LPascholatti <lucaspascholatti@gmail.com>
2020-01-09 16:14:57 +0100

    Add limitation to Snake movement inside canvas

--- snake.js
+++ snake.js
@@ -12,6 +12,19 @@ function Snake() {
   this.update = function() {
     this.x += this.xSpeed;
     this.y += this.ySpeed;
+
+    if (this.x > canvas.width) {
+      this.x = 0;
+    }
+    if (this.y > canvas.height) {
+      this.y = 0;
+    }
+    if (this.x < 0) {
+      this.x = canvas.width;
+    }
+    if (this.y < 0) {
+      this.y = canvas.height;
+    }
   }
 
   this.changeDirection = function(direction) {
